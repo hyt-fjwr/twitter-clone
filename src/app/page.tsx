e2ec18d2843debd3154420fd192b4e5c3d0378ab
crow-editing <f.hayato0104@gmail.com>
2023-08-31 23:09:01 +0900

    A

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='w-full h-hull flex justify-center items-center relative'>
+    <div className='w-full h-hull flex justify-center items-center relative bg-black'>
       <div className='max-w-screen-lg w-ful h-full flex relative'>
-      
+        <section className='fixed w-72 flex flex-col h-screen'>
+          {
+            NAVIGATION_ITEMS.map((item)=>{
+              <div key={item.title}>
+                
+              </div>
+            })
+          }
+        </section>
+        <main>HOme timeline</main>
+        <section>right section</section>
       </div>
     </div>
   )
